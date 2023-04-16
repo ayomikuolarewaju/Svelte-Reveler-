b601585614ee5a24c6a509f05ef75f0f3faa8116
Ayomiku Almaroof Olarewaju <53964338+ayomikuolarewaju@users.noreply.github.com>
2023-04-16 11:13:57 +0100

    Add files via upload

---
+++ src/routes/+page.server.js
@@ -0,0 +1,56 @@
+export const load=()=>{
+    const products =[
+        {
+			"id": 1,
+			"title": "iPhone 9",
+			"description": "An apple mobile which is nothing like apple",
+			
+		},
+		{
+			"id": 2,
+			"title": "iPhone X",
+			"description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+			
+		},
+		{
+			"id": 3,
+			"title": "Samsung Universe 9",
+			"description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+			
+		},
+		{
+			"id": 4,
+			"title": "OPPOF19",
+			"description": "OPPO F19 is officially announced on April 2021.",
+			
+		},
+		{
+			"id": 7,
+			"title": "Samsung Galaxy Book",
+			"description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+			
+		},
+		{
+			"id": 8,
+			"title": "Microsoft Surface Laptop 4",
+			"description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+			
+		},
+		{
+			"id": 9,
+			"title": "Infinix INBOOK",
+			"description": "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+			
+		},
+		{
+			"id": 10,
+			"title": "HP Pavilion 15-DK1056WM",
+			"description": "HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10",
+			
+		},
+    ];
+       	
+    return{
+        products
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import img from './img/down-arrow.png'
+    export let data;
+    const product = data.products
+
+    let rev = true
+    
+    const reveler =(id)=>{
+        const pdi = product.filter((p)=>p.id === id)
+        let i = pdi[0].id
+        if(i === id){
+           rev = !rev;
+        }
+    }
+
+</script>
+
+
+
+<div class="h-auto p-10 bg-gray-200">
+
+    <div class="container mx-auto">
+        {#each product as pd (pd.id)}
+            {#if rev}
+           <div class="flex flex-row justify-between  bg-purple-600  p-2 m-2 space-x-5 w-2/3 mx-auto">
+            <h3 class=" text-2xl text-white">{pd.title}</h3>
+            <img src="{img}" class="w-16 h-16 text-white" on:click={()=>{reveler(pd.id)}} alt="revelers" on:keydown={()=>{reveler(pd.id)}}>
+           </div>
+           <div class="hidden p-2 m-2 w-2/3 mx-auto">
+            <p class="">{pd.description}</p>
+           </div>
+           {:else}
+           <div class="flex flex-row justify-between  bg-purple-600  p-2 m-2 space-x-5 w-2/3 mx-auto">
+               <h3 class="text-2xl text-white bg-purple-600">{pd.title}</h3>
+               <img src="{img}" class="w-16 h-16 text-white" on:click={()=>{reveler(pd.id)}} alt="revelers" on:keydown={()=>{reveler(pd.id)}}>
+          </div>
+          <div class="flex p-2 m-2 w-2/3 mx-auto transition delay-1000 bg-gray-500 text-white">
+            <p class="">{pd.description}</p>
+          </div>
+            {/if}
+        {/each}
+    </div>
+
+</div>
